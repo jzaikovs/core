ad7eb9e02f308f2189f29e38c2ff8e0c4773bdfe
Jānis Zaikovs <jzaikovs@gmail.com>
2014-11-01 15:25:33 +0200

    adding json mime to content handler

--- content.go
+++ content.go
@@ -7,9 +7,13 @@ import (
 	"path/filepath"
 )
 
+func init() {
+	mime.AddExtensionType(".json", MIME_JSON)
+}
+
 //	NOTE: this is just for development, file handling (CDN) better done by nginx or other
 func ServeFile(out Output, path string) {
-
+	//Log.Info("serving file:", path)
 	f, err := os.OpenFile("./www/"+path, os.O_RDONLY, 0)
 	if err != nil {
 		out.Response(Response_Not_Found)
